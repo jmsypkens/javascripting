3358c3c33dd8d4ac5729e6d78b024221752e866f
Jerry Sypkens <lovehalfblack@gmail.com>
2016-03-11 23:14:42 -0800

    adding javascripting files

---
+++ function-arguments.js
@@ -0,0 +1,5 @@
+function math (arguOne, arguTwo, arguThree) { // It's important for you to understand that arguments names are only used to reference them.
+    return (arguTwo * arguThree) + arguOne; 
+}
+
+console.log(math(53,61,67)); 
